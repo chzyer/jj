682fcbb26e48e326a26474b7e29afb5f42923c01
Cheney <me@chenye.org>
2015-06-20 22:13:20 +0800

    change uuid

--- internal/utils.go
+++ internal/utils.go
@@ -1,14 +1,13 @@
 package internal
 
 import (
-	"crypto/md5"
-	"encoding/base64"
 	"math/rand"
 	"time"
+
+	"github.com/satori/go.uuid"
 )
 
 var (
-	salt    = []byte("e448579a-142c-11e5-93fa-525424be6a56")
 	randNum = rand.New(rand.NewSource(time.Now().Unix()))
 )
 
@@ -16,14 +15,6 @@ func Rand() *rand.Rand {
 	return randNum
 }
 
-func GenUuid(secret []byte) string {
-	h := md5.New()
-	h.Write(salt)
-	h.Write([]byte(secret))
-	randByte := make([]byte, 8)
-	for i := 0; i < 8; i++ {
-		randByte[i] = byte(rand.Intn(256))
-	}
-	h.Write(randByte)
-	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+func GenUuid() string {
+	return string(uuid.NewV4()) + string(uuid.NewV4())
 }
