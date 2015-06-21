8e139bd88e16ce801afb40763b45c5cdc2814a08
Cheney <me@chenye.org>
2015-06-22 00:33:24 +0800

    update

--- rpc/rpcprot/v1.go
+++ rpc/rpcprot/v1.go
@@ -51,7 +51,7 @@ func (p1 *ProtocolV1) Read(buf *bytes.Buffer, metaEnc rpc.Encoding, p *Packet) e
 	}
 
 	if err := metaEnc.Decode(buf, &p.Meta); err != nil {
-		return logex.Trace(err, buf.String())
+		return logex.Trace(err, length, buf.Bytes())
 	}
 
 	p.Data = NewRawData(buf.Bytes())
