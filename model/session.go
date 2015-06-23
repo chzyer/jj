1cbb9e22bc08d8aa86e2ac0a04083a72e7a20d09
Cheney <me@chenye.org>
2015-06-23 09:12:12 +0800

    add NewSession

--- model/session.go
+++ model/session.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"time"
+
 	"gopkg.in/logex.v1"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -10,9 +12,10 @@ var (
 )
 
 type Session struct {
-	Id     bson.ObjectId `bson:"_id" json:"id"`
-	UserId string        `bson:"userid" json:"userid"`
-	To     string        `bson:"to" json:"to"`
+	Id       bson.ObjectId `bson:"_id" json:"id"`
+	UserId   string        `bson:"userid" json:"userid"`
+	ToUid    string        `bson:"touid" json:"touid"`
+	LastDate time.Time     `bson:"lastDate" json:"lastDate"`
 }
 
 type SessionModel struct {
@@ -21,6 +24,33 @@ type SessionModel struct {
 
 func NewSessionModel(mdb *Mdb) *SessionModel {
 	return &SessionModel{NewBaseModel(mdb, Session{})}
+}
+
+func (sm *SessionModel) New(uid, toUid string) (s *Session, err error) {
+	if !bson.IsObjectIdHex(uid) || !bson.IsObjectIdHex(toUid) {
+		return nil, ErrUserInvalidUid.Trace()
+	}
+
+	err = logex.Trace(sm.One(M{
+		"uid":   uid,
+		"touid": toUid,
+	}, &s))
+	if err != nil {
+		return nil, logex.Trace(err)
+	}
+
+	if s != nil {
+		return
+	}
+
+	if err := logex.Trace(sm.Upsert(M{
+		"uid":   uid,
+		"touid": toUid,
+	}, M{"$setOnInsert": M{
+		"": "",
+	}})); err != nil {
+		return
+	}
 }
 
 func (sm *SessionModel) Get(sessionId string) (s *Session, err error) {
